c7e5ca3c656c226d857424e671990ad7e2ed8fc0
matiasonnelainen <monnelainen@gmail.com>
2022-01-30 23:01:39 +0200

    Fixes

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
 CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT);
 CREATE TABLE settings (id SERIAL PRIMARY KEY, user_id INTEGER REFERENCES users, name TEXT, language TEXT);
-CREATE TABLE averages (id SERIAL PRIMARY KEY, game_id INTEGER REFERENCES games, average_date DATE, user_id INTEGER REFERENCES users, average REAL);
 CREATE TABLE games (id SERIAL PRIMARY KEY, game_date DATE, player_one INTEGER REFERENCES users, player_two INTEGER REFERENCES users, result INTEGER);
+CREATE TABLE averages (id SERIAL PRIMARY KEY, game_id INTEGER REFERENCES games, average_date DATE, user_id INTEGER REFERENCES users, average REAL);

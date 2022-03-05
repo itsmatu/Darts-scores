c32e74b1544697eef8ecc2e866a55bdb4a8b75ef
matiasonnelainen <monnelainen@gmail.com>
2022-03-05 22:00:34 +0200

    Work on UI

--- routes.py
+++ routes.py
@@ -14,7 +14,10 @@ def index():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "GET":
-        return render_template("register.html")
+        if session.get("username"):
+            return redirect("/")
+        else:
+            return render_template("register.html")
     if request.method == "POST":
         username = request.form["username"]
         password1 = request.form["password1"]
@@ -27,7 +30,10 @@ def register():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "GET":
-        return render_template("login.html")
+        if session.get("username"):
+            return redirect("/")
+        else:
+            return render_template("login.html")
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]

b85dccd4f3b6aaeb750944cd725f57fc8434a923
matiasonnelainen <monnelainen@gmail.com>
2022-03-06 20:26:03 +0200

    Fixed users.py

--- users.py
+++ users.py
@@ -2,6 +2,7 @@ from app import app
 from db import db
 from flask import render_template, redirect, request, session
 from werkzeug.security import check_password_hash, generate_password_hash
+import secrets
 
 def login(username, password):
     sql = "SELECT id, password FROM users WHERE username=:username"
